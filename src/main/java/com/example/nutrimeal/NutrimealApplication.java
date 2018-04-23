7c357d06992e9199d5624f0e907648bc3d20d2e9
clement-bey-cgi <clement.bey@ipilyon.net>
2018-04-23 02:40:43 +0200

    Initialisation du projet

---
+++ src/main/java/com/example/nutrimeal/NutrimealApplication.java
@@ -0,0 +1,12 @@
+package com.example.nutrimeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrimealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrimealApplication.class, args);
+	}
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+drop table student IF EXISTS;
+create table student
+(
+   id integer not null,
+   name varchar(255) not null,
+   passport_number varchar(255) not null,
+   primary key(id)
+);
