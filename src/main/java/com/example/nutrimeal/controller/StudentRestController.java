a92717f809503feae28d5d484aa0e02724d98612
BEY <clement.bey@cgi.com>
2018-04-24 10:24:34 +0200

    Initialisation terminée

--- src/main/java/com/example/nutrimeal/controller/StudentRestController.java
+++ src/main/java/com/example/nutrimeal/controller/StudentRestController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,10 +33,8 @@ public class StudentRestController {
 		}
 	}
 	
-	@RequestMapping(value = "/create", method = RequestMethod.GET, produces = "application/json")
-	public ResponseEntity<?> create(){
-		Student student = new Student();
-		student.setNom("Popol");
+	@RequestMapping(value = "/create", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+	public ResponseEntity<?> create(@RequestBody Student student){
 		Student persistedStudent = studentService.save(student);
 		return new ResponseEntity<>(persistedStudent, HttpStatus.CREATED);
 	}
