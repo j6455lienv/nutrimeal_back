3bfd011d32cbbd75422cb9e28eab9f0984bc0a46
Shingo76 <gaetan.inidjel@laposte.net>
2018-07-04 10:36:26 +0200

    Clean code

--- src/main/java/com/example/nutrimeal/controller/RecetteRestController.java
+++ src/main/java/com/example/nutrimeal/controller/RecetteRestController.java
@@ -3,16 +3,12 @@ package com.example.nutrimeal.controller;
 import java.util.Optional;
 import java.util.Set;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
