1b4594aaeb2c8f83cac2d367b76f0e73c62f2fad
j6455lienv <venet.julien@gmail.com>
2018-10-10 11:02:16 +0200

    add_security_wip

--- src/main/java/com/example/nutrimeal/security/SimpleCorsFilter.java
+++ src/main/java/com/example/nutrimeal/security/SimpleCorsFilter.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -14,7 +15,7 @@ import java.io.IOException;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class SimpleCorsFilter implements Filter {
 
-  @Value("${spring.cors.allowOrigin}")
+  @Value("https://j6455lienv.github.io")
   private String allowOrigin;
 
   public SimpleCorsFilter() {
